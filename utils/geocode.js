adf71f756da57ea24fa0f086dedf08760787a468
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-30 13:36:30 -0500

    fetching to api works'

--- utils/geocode.js
+++ utils/geocode.js
@@ -7,5 +7,18 @@ const request = require('request');
 const geocode = (location, fn) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
     location
-  )}.json?access_token=${GEOCODE_TOKEN}&limit=1`;
+  )}.json?access_token=${process.env.GEOCODE_TOKEN}&limit=1`;
+
+  request({ url, json: true }, (error, res) => {
+    if (error) {
+      console.log('Unable to connect to location services.');
+    }
+    if (res.body.features) {
+      console.log(res.body.features);
+    } else {
+      console.log('Error: Could not find location.');
+    }
+  });
 };
+
+geocode('baltimore');

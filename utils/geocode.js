33d77410aebb303d4fcda59b2cb22214070825bd
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-02-01 11:45:02 -0500

    installed reqpromise, changed geocode to use request-promise

--- utils/geocode.js
+++ utils/geocode.js
@@ -1,43 +1,25 @@
 require('dotenv').config();
-const request = require('request');
+const reqPromise = require('request-promise');
 
 const geocode = (location, fn) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
     location
   )}.json?access_token=${process.env.GEOCODE_TOKEN}&limit=1`;
 
-  request({ url, json: true }, (err, res) => {
-    if (err) {
+  reqPromise({ url, json: true })
+    .then(res => {
+      const [longitude, latitude] = res.features[0].center;
+      const location = res.features[0].place_name;
+      fn(undefined, { longitude, latitude, location });
+    })
+    .catch(err => {
       fn(
         (error = {
-          message: 'Could not connect to location services'
+          message: `${err.statusCode} Could not connect to location services.`
         }),
         {}
       );
-    }
-    if (res.body.message || res.body.features.length === 0) {
-      fn(
-        (error = {
-          message: 'Could not find location'
-        }),
-        {}
-      );
-    } else {
-      fn(undefined, {
-        longitude: res.body.features[0].center[0],
-        latitude: res.body.features[0].center[1],
-        location: res.body.features[0].place_name
-      });
-    }
-  });
+    });
 };
 
 module.exports = geocode;
-
-// const getgeo = () => {
-//   geocode(';', (error, { longitude, latitude, location }) => {
-//     console.log(error, longitude, latitude, location);
-//   });
-// };
-
-// getgeo();
